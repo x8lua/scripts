7d2f0a492df93f1d7be6edfcaca9206f45e5843d
x8lua <155454885+x8lua@users.noreply.github.com>
2026-08-17 16:54:50 +0800

    Update gakuran_fling.lua

--- gakuran_fling.lua
+++ gakuran_fling.lua
@@ -10,7 +10,7 @@ if _G.LarpkuranRereUI and _G.LarpkuranRereUI.Shutdown then
     pcall(_G.LarpkuranRereUI.Shutdown)
 end
 
-local source = game:HttpGet("https://raw.githubusercontent.com/x8lua/Rere/v0.1.24/src/Rere.lua")
+local source = game:HttpGet("https://raw.githubusercontent.com/x8lua/Rere/v0.1.25/src/Rere.lua")
 local Rere = assert(compiler(source))()
 Rere.Init()
 _G.LarpkuranRereUI = Rere
